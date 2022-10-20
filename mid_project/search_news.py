d1e0109073c2f5ce72e15fefde477b09df74ca6e
Nonamedcoder <76687548+skagkdbs123@users.noreply.github.com>
2022-10-20 17:32:07 +0900

    Update search_news.py

--- mid_project/search_news.py
+++ mid_project/search_news.py
@@ -31,6 +31,6 @@ num=1
 for i in naver:
     components.iframe(f"{i}",width=800, height=300, scrolling=False)
     with st.expander(f'{keyword}'+' 검색결과'+f'{num}'):
-        components.iframe(f"{i}",width=600, height=400, scrolling=True)
+        components.iframe(f"{i}",width=1200, height=900, scrolling=True)
     num+=1
 print(Naver_news(keyword))
