4eb58a39e09ecc7cee600a9acb75398c45c0a764
Nonamedcoder <76687548+skagkdbs123@users.noreply.github.com>
2022-10-20 15:22:41 +0900

    Update test7.py

--- test7.py
+++ test7.py
@@ -2,4 +2,4 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 # embed streamlit docs in a streamlit app
-components.iframe("https://docs.streamlit.io/en/latest")
+components.iframe("https://docs.streamlit.io/en/latest",width=800, height=1200, scrolling=True)
