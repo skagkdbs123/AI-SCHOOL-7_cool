23a506a51bef1fb0b05deddc82612c453bbaa234
Nonamedcoder <76687548+skagkdbs123@users.noreply.github.com>
2022-10-21 20:13:45 +0900

    Update test7.py

--- test7.py
+++ test7.py
@@ -5,5 +5,3 @@ import streamlit.components.v1 as components
 option = st.selectbox(
     'How would you like to be contacted?',
     ('Email', 'Home phone', 'Mobile phone'))
-
-components.iframe("https://m.khan.co.kr/?tab=home",width=800, height=1200, scrolling=True)
