790bd0f4f2f293fb6b9bd67db2e7666d67428b42
Nonamedcoder <76687548+skagkdbs123@users.noreply.github.com>
2022-10-21 20:06:58 +0900

    Update test7.py

--- test7.py
+++ test7.py
@@ -2,4 +2,4 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 # embed streamlit docs in a streamlit app
-components.iframe("https://velog.io/@dlsdk2526/%EC%A0%95%EC%B2%98%EA%B8%B0-%EC%8B%A4%EA%B8%B0-%EC%9A%94%EC%95%BD-10-%EC%95%A0%ED%94%8C%EB%A6%AC%EC%BC%80%EC%9D%B4%EC%85%98-%ED%85%8C%EC%8A%A4%ED%8A%B8/embed",width=800, height=1200, scrolling=True)
+components.iframe("https://discuss.streamlit.io/t/how-to-use-dropdown-with-url-query-parameter/13337/2",width=800, height=1200, scrolling=True)
