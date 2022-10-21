0b5cb6f1d931f9f1bf221f9f4d5b14cb75b500a9
Nonamedcoder <76687548+skagkdbs123@users.noreply.github.com>
2022-10-21 22:19:17 +0900

    Update search_news.py

--- mid_project/search_news.py
+++ mid_project/search_news.py
@@ -42,13 +42,28 @@ def Naver_news(keyword):
         top_page_url.append(temp)
         
     return top_page_url
+
+def Naver_title(keyword):
+    
+    real_title=[]
+    enctext = urllib.parse.quote(keyword)
+    url = 'https://search.naver.com/search.naver?where=news&ie=utf8&sm=nws_hty&query='+enctext
+    headers = {'User-Agent': 'Chrome/105.0.0.0 '}
+    response = requests.get(url, headers=headers)
+    soup = bs(response.text, "html.parser")
+    items = soup.select('.news_tit')
+    for item in items:
+        real_title.append(item.text)
+        
+    return real_title
 # def show_page(i):
 #     components.iframe(f"{i}",width=800, height=1200, scrolling=True)
 # number_list=[1,2,3,4,5,6,7,8,9,10]
 
 # dic = { name:value for name, value in zip(number_list,i)}
 
-naver = Naver_news(keyword)
+naver_link = Naver_news(keyword)
+naver_main = Naver_title(keyword)
 num=1
 for i in naver:
     with st.expander(f'{keyword}'+' 검색결과'+f'{num}'):
