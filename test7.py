14904542aee2dfc926b9740e68a76b5ccc86886b
Nonamedcoder <76687548+skagkdbs123@users.noreply.github.com>
2022-10-21 20:08:31 +0900

    Update test7.py

--- test7.py
+++ test7.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-# embed streamlit docs in a streamlit app
+
+option = st.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone'))
+
 components.iframe("https://docs.streamlit.io/library/api-reference",width=800, height=1200, scrolling=True)
