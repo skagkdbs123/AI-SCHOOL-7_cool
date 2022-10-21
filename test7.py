7a56c226200a70d7acd759b5b718bcc25ac95727
Nonamedcoder <76687548+skagkdbs123@users.noreply.github.com>
2022-10-21 11:32:06 +0900

    Update test7.py

--- test7.py
+++ test7.py
@@ -2,4 +2,4 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 # embed streamlit docs in a streamlit app
-components.iframe("https://tre2man.tistory.com/188",width=800, height=1200, scrolling=True)
+components.iframe("https://n.news.naver.com/mnews/article/021/0002536857?sid=101/embed",width=800, height=1200, scrolling=True)
