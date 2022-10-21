34372d80c1ae09b743feeea995eb7aa7d76b86ae
Nonamedcoder <76687548+skagkdbs123@users.noreply.github.com>
2022-10-21 20:07:55 +0900

    Update test7.py

--- test7.py
+++ test7.py
@@ -2,4 +2,4 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 # embed streamlit docs in a streamlit app
-components.iframe("https://discuss.streamlit.io/t/how-to-use-dropdown-with-url-query-parameter/13337/2",width=800, height=1200, scrolling=True)
+components.iframe("https://docs.streamlit.io/library/api-reference",width=800, height=1200, scrolling=True)
